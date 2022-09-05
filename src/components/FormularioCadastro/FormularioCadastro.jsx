b37fe6552875c630802f2fa66ef747c5b1941802
artenlf <felipe.arten@gmail.com>
2022-09-05 17:38:23 -0300

    Códigos Aula 19

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,3 +1,4 @@
+import { Step, StepLabel, Stepper, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { useEffect } from "react";
 import DadosDeEntrega from "./DadosDeEntrega";
@@ -8,13 +9,16 @@ function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const [dadosColetados, setDados] = useState({});
   useEffect(() => {
-    console.log(dadosColetados);
+    if (etapaAtual === formularios.length - 1) {
+      aoEnviar(dadosColetados);
+    }
   });
 
   const formularios = [
     <DadosUsuario aoEnviar={coletarDados} />,
     <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
     <DadosDeEntrega aoEnviar={coletarDados} />,
+    <Typography variant="h5">Obrigado pelo seu cadastro!</Typography>,
   ];
 
   function coletarDados(dados) {
@@ -26,7 +30,25 @@ function FormularioCadastro({ aoEnviar, validarCPF }) {
     setEtapaAtual(etapaAtual + 1);
   }
 
-  return <>{formularios[etapaAtual]}</>;
+  return (
+    <>
+      <Stepper activeStep={etapaAtual}>
+        <Step>
+          <StepLabel>login</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>pessoal</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>entrega</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>finalização</StepLabel>
+        </Step>
+      </Stepper>
+      {formularios[etapaAtual]}
+    </>
+  );
 }
 
 export default FormularioCadastro;
